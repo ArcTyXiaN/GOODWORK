2abfac1c86f5aecd1c5c82179c0878454e35b051
ArcTyXiaN <obaloluwao008@gmail.com>
2025-11-21 20:20:26 +0100

    resolving why it is not deploying on cloud servers

--- FRONTEND/src/api/jobs.js
+++ FRONTEND/src/api/jobs.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL; // e.g., http://localhost:5000/api
+const API_URL = import.meta.env.VITE_API_URL; 
 
 // ----------------- AUTH -----------------
 export const registerUser = async (user) => {
